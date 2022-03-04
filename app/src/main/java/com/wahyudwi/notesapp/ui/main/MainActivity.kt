bed14a966ea9856f743966f3193efa44ef115a13
Wahyu Dwe <wahyu5161.w5@gmail.com>
2022-03-04 18:43:42 +0700

    first commit

---
+++ app/src/main/java/com/wahyudwi/notesapp/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.wahyudwi.notesapp.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.wahyudwi.notesapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
